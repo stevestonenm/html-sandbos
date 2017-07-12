654051df714a88c35518c591d825f6ca6abcd16e
Steve Stone <stoneflynm@gmail.com>
2017-07-12 14:00:11 -0600

    added table ul and ol

--- index.php
+++ index.php
@@ -6,5 +6,43 @@
 	</head>
 	<body>
 		<h1>This is my webpage!</h1>
+
+		<table>
+			<tr>
+				<th>Cats</th>
+				<th>Dogs</th>
+			</tr>
+			<tr>
+				<td>Persian</td>
+				<td>Beagle</td>
+			</tr>
+			<tr>
+				<td>Maine Coon</td>
+				<td>Rottweiler</td>
+			</tr>
+			<tr>
+				<td>Siamese</td>
+				<td>German Shephard</td>
+			</tr>
+		</table>
+
+		<!--This is an html comment-->
+
+		<h2>A Few of My Favorite Things</h2>
+		<ul>
+			<li>Tacos</li>
+			<li>The Beach</li>
+			<li>Kittens</li>
+			<li>Fast Cars</li>
+		</ul>
+		<h2>Things to Do Today</h2>
+		<ol>
+			<li>Feed the Kids</li>
+			<li>Go for a Run</li>
+			<li>Take a Shower</li>
+			<li>Water the Plants</li>
+		</ol>
+
+
 	</body>
 </html>
