14e5ca4aed67af5f7ea3e93fdeaaea1b56746426
Steve Stone <stoneflynm@gmail.com>
2017-07-31 10:23:27 -0600

    added lots of practice, to long since last commit

--- index.php
+++ index.php
@@ -31,6 +31,60 @@
 		<title>Bootstrap Practice</title>
 	</head>
 	<body>
-		<p>Hello</p>
+		<header>
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-12">
+						<h1>Hello Bootstrap</h1>
+					</div>
+				</div>
+
+			</div>
+		</header>
+		<!-- begin grid layout-->
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6">
+					<h2>Column 1</h2>
+					<p>
+						It indicates a synchronic distortion in the areas emanating triolic waves. The cerebellum, the cerebral cortex, the brain stem,  the entire nervous system has been depleted of electrochemical energy. Any device like that would produce high levels of triolic waves. These walls have undergone some kind of selective molecular polarization. I haven't determined if our phaser energy can generate a stable field. We could alter the photons with phase discriminators.
+					</p>
+				</div>
+				<div class="col-md-6">
+					<h2>Column 2</h2>
+					<p>
+						Sensors indicate human life forms 30 meters below the planet's surface. Stellar flares are increasing in magnitude and frequency. Set course for Rhomboid Dronegar 006, warp seven. There's no evidence of an advanced communication network. Total guidance system failure, with less than 24 hours' reserve power. Shield effectiveness has been reduced 12 percent. We have covered the area in a spherical pattern which a ship without warp drive could cross in the given time.
+					</p>
+				</div><!--closes /.col-md-6-->
+			</div><!--closes /.row-->
+
+			<div class="row">
+				<div class="col-md-12">
+					<h2>Column 3</h2>
+					<p>
+						Exceeding reaction chamber thermal limit. We have begun power-supply calibration. Force fields have been established on all turbo lifts and crawlways. Computer, run a level-two diagnostic on warp-drive systems. Antimatter containment positive. Warp drive within normal parameters. I read an ion trail characteristic of a freighter escape pod. The bomb had a molecular-decay detonator. Detecting some unusual fluctuations in subspace frequencies.
+					</p>
+				</div><!--closes /.col-md-12-->
+			</div><!--closes /.row-->
+			<div class="col-md-4">
+				<h2>Column 4</h2>
+				<p>
+					I have reset the sensors to scan for frequencies outside the usual range. By emitting harmonic vibrations to shatter the lattices. We will monitor and adjust the frequency of the resonators. He has this ability of instantly interpreting and extrapolating any verbal communication he hears. It may be due to the envelope over the structure, causing hydrogen-carbon helix patterns throughout. I'm comparing the molecular integrity of that bubble against our phasers.
+				</p>
+			</div>
+			<div class="col-md-4">
+				<h2>Column 5</h2>
+				<p>
+					Deflector power at maximum. Energy discharge in six seconds. Warp reactor core primary coolant failure. Fluctuate phaser resonance frequencies. Resistance is futile. Recommend we adjust shield harmonics to the upper EM band when proceeding. These appear to be some kind of power-wave-guide conduits which allow them to work collectively as they perform ship functions. Increase deflector modulation to upper frequency band.
+				</p>
+			</div>
+			<div class="col-md-4">
+				<h2>Column 6</h2>
+				<p>
+					Now what are the possibilities of warp drive? Cmdr Riker's nervous system has been invaded by an unknown microorganism. The organisms fuse to the nerve, intertwining at the molecular level. That's why the transporter's biofilters couldn't extract it. The vertex waves show a K-complex corresponding to an REM state. The engineering section's critical. Destruction is imminent. Their robes contain ultritium, highly explosive, virtually undetectable by your transporter.
+				</p>
+			</div>
+
+		</div>
 	</body>
 </html>
