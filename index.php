644b8298dedd5e40ecd1fc4544372196e5f45ebf
Steve Stone <stoneflynm@gmail.com>
2017-07-31 13:16:31 -0600

    working on form

--- index.php
+++ index.php
@@ -32,14 +32,59 @@
 	</head>
 	<body>
 		<header>
-			<div class="container">
-				<div class="row">
-					<div class="col-xs-12">
-						<h1>Hello Bootstrap</h1>
+			<nav class="navbar navbar-default">
+				<div class="container">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-nav" aria-expanded="false">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand" href="#">Bootstrap Sandbox</a>
 					</div>
-				</div>
 
-			</div>
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse" id="main-nav">
+						<ul class="nav navbar-nav">
+							<li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+							<li><a href="#">Link</a></li>
+							<li class="dropdown">
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+								<ul class="dropdown-menu">
+									<li><a href="#">Action</a></li>
+									<li><a href="#">Another action</a></li>
+									<li><a href="#">Something else here</a></li>
+									<li role="separator" class="divider"></li>
+									<li><a href="#">Separated link</a></li>
+									<li role="separator" class="divider"></li>
+									<li><a href="#">One more separated link</a></li>
+								</ul>
+							</li>
+						</ul>
+						<form class="navbar-form navbar-left">
+							<div class="form-group">
+								<input type="text" class="form-control" placeholder="Search">
+							</div>
+							<button type="submit" class="btn btn-default">Submit</button>
+						</form>
+						<ul class="nav navbar-nav navbar-right">
+							<li><a href="#">Link</a></li>
+							<li class="dropdown">
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+								<ul class="dropdown-menu">
+									<li><a href="#">Action</a></li>
+									<li><a href="#">Another action</a></li>
+									<li><a href="#">Something else here</a></li>
+									<li role="separator" class="divider"></li>
+									<li><a href="#">Separated link</a></li>
+								</ul>
+							</li>
+						</ul>
+					</div><!-- /.navbar-collapse -->
+				</div><!-- /.container-fluid -->
+			</nav>
 		</header>
 		<!-- begin grid layout-->
 		<div class="container">
@@ -85,6 +130,32 @@
 				</p>
 			</div>
 
+			<div class="row">
+				<div class="col-md-4">
+					<!--begin form-->
+					<form>
+						<div class="form-group">
+							<label for="txtName">Your Name</label>
+							<input id="txtName" name="txtName" class="form-control" type="text" placeholder="Name">
+						</div>
+						<div class="form-group">
+							<input class="form-control" type="text" placeholder="Email Address">
+						</div>
+						<div class="form-group">
+							<input class="form-control" type="text" placeholder="Subject">
+						</div>
+						<div class="form-group">
+							<textarea class="form-control"cols="30" rows="10"></textarea>
+						</div>
+						<button type="submit" class="btn btn-default">Submit</button>
+					</form>
+				</div>
+				<div class="col-md-7 col-md-offset-1"
+			</div>
+
 		</div>
+		<footer>
+
+		</footer>
 	</body>
 </html>
